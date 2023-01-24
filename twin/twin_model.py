7604ef3bf9a27538a5a927a477058e471629aa53
griggle <grigglethewise@gmail.com>
2023-01-24 22:20:39 +0000

    (feature) added position updating and extra sensors

--- twin/twin_model.py
+++ twin/twin_model.py
@@ -1,10 +1,15 @@
 from copy import deepcopy
+from time import time_ns
 
 
 class TwinModel:
     def __init__(self):
         # print("created new twin")
 
+        # META INFO
+        self.last_update_time = -1
+
+        # POSITIONAL INFO
         self.x_pos = 0  # relative x-axis displacement from start position
         self.y_pos = 0  # relative y-axis displacement from start position
         self.z_pos = 0  # relative z-axis displacement from start position
@@ -13,11 +18,20 @@ class TwinModel:
         self.y_vel = 0  # y-axis velocity from start position
         self.z_vel = 0  # z-axis velocity from start position
 
-        self.x_rot = 0  # x-axis rotation from upright
-        self.y_rot = 0  # y-axis rotation from upright
-        self.z_rot = 0  # z-axis rotation from upright
+        self.x_acc = 0  # x-axis acceleration from start position (read directly from sensors)
+        self.y_acc = 0  # y-axis acceleration from start position (read directly from sensors)
+        self.z_acc = 0  # z-axis acceleration from start position (read directly from sensors)
 
-        # TODO: add sensor information to the twin storage when available
+        self.x_rot = 0  # x-axis rotation from upright (read directly from sensors)
+        self.y_rot = 0  # y-axis rotation from upright (read directly from sensors)
+        self.z_rot = 0  # z-axis rotation from upright (read directly from sensors)
+
+        # SENSORS
+        self.distance_sensor = 0  # number of mm to the nearest object ahead of the sensor
+        self.motor0_rot = 0  # rotation in degrees of motor 0
+        self.motor1_rot = 0  # rotation in degrees of motor 1
+        self.motor2_rot = 0  # rotation in degrees of motor 2
+        self.motor3_rot = 0  # rotation in degrees of motor 3
 
     def copy(self):
         # copy function for making predictions
@@ -25,7 +39,29 @@ class TwinModel:
 
     def update(self, sensor_data, instruction, environment):
         # update state based upon truths and environment
-        pass
+
+        # TODO: Update sensors when we have a structure for them
+
+        # UPDATE POSITION
+        # if we are not on our first update
+        if self.last_update_time != -1:
+            # time difference since last update in seconds
+            time_delta = (time_ns() - self.last_update_time) / 1_000_000_000
+            if time_delta == 0:
+                time_delta = 0.000000001
+
+            # acceleration and rotation already updated with sensor update
+
+            self.x_vel += self.x_acc * time_delta
+            self.y_vel += self.y_acc * time_delta
+            self.z_vel += self.z_acc * time_delta
+
+            self.x_pos += self.x_vel * time_delta
+            self.y_pos += self.y_vel * time_delta
+            self.z_pos += self.z_vel * time_delta
+
+        # reset clock
+        self.last_update_time = time_ns()
 
     def predict_next(self, n=1, environment=None, instruction=None):
         # predict based upon a possible new instruction and simulated sensor data gathered
@@ -40,10 +76,14 @@ class TwinModel:
 
             # PREDICTION AREA
 
-            # simple velocity example
-            prediction.x_pos += prediction.x_vel
-            prediction.y_pos += prediction.y_vel
-            prediction.z_pos += prediction.z_vel
+            # simple movement example
+            self.x_vel += self.x_acc
+            self.y_vel += self.y_acc
+            self.z_vel += self.z_acc
+
+            self.x_pos += self.x_vel
+            self.y_pos += self.y_vel
+            self.z_pos += self.z_vel
 
             # END OF PREDICTION AREA
 
