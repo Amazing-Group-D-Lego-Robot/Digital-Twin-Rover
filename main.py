86c3279f2b39b19c61228c2716d83de3aa26aa28
griggle <grigglethewise@gmail.com>
2023-01-26 18:47:16 +0000

    (feature) twin -> server -> visualisation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # ENTRY POINT FOR WHOLE SYSTEM
+import random
+from time import sleep
 
 from controller.controller import Controller
 
@@ -7,8 +9,25 @@ def main():
     controller = Controller()
 
     # Proof of concept connection to show the twin server can send data
-    # while True:
-    #     controller.twin.update()
+
+    controller.twin.worldstate.twin.x_acc = 0.1
+    controller.twin.worldstate.twin.y_acc = 0.2
+    controller.twin.worldstate.twin.z_acc = 0.3
+
+    i = 0
+
+    while True:
+        if i % 100 == 0:
+            controller.twin.worldstate.twin.x_acc = -0.5 + random.random()
+            controller.twin.worldstate.twin.y_acc = -0.5 + random.random()
+            controller.twin.worldstate.twin.z_acc = -0.5 + random.random()
+            print("direction change")
+            i = 0
+
+        i += 1
+
+        controller.twin.update()
+        sleep(0.1)
 
 
 if __name__ == "__main__":
--- twin/twin.py
+++ twin/twin.py
@@ -1,33 +1,7 @@
 # whole twin system, environment and all
 
 from twin.worldstate import WorldState
-
-from ursinanetworking import *
-from random import randint
-from time import sleep
-
-Server = UrsinaNetworkingServer("localhost", 25565)
-
-
-@Server.event
-def onClientConnected(client):
-    print(f"{client} connected !")
-    print(f"Current clients: {Server.get_clients()}")
-
-    for _ in range(0, 10):
-        random_position()
-        sleep(1)
-
-
-@Server.event
-def onClientDisconnected(client):
-    print(f"{client} disconnected !")
-
-
-def random_position():
-    position = (randint(0, 20), 0, randint(0, 20))
-    print("Position for client is", position)
-    Server.broadcast("update_pos", position)
+from visualisation.server import TwinServer
 
 
 class TwinSystem:
@@ -35,18 +9,19 @@ class TwinSystem:
         # print("created a new twin system")
 
         self.worldstate = WorldState()
+        self.server = TwinServer()
 
     def update(self, sensor_info=None, instruction=None):
         # update the rover with new instruction and sensor info
         # return new updated world state
 
         self.worldstate.twin.update(sensor_info, instruction, self.worldstate.environment)
-
-        Server.process_net_events()
+        self.server.update(self.worldstate)
 
     def predict_next(self, n=1, instruction=None):
         # predict next n time steps of the twin model and returns them with the current environment
 
         return \
-            self.worldstate.twin.predict_next(n=n, environment=self.worldstate.environment, instruction=instruction), \
-                self.worldstate.environment.copy()
+            self.worldstate.twin.predict_next(
+                n=n, environment=self.worldstate.environment,
+                instruction=instruction), self.worldstate.environment.copy()
