9fa5471be7ddd143040a404a8af90e1a720dab94
griggle <grigglethewise@gmail.com>
2023-01-26 14:12:19 +0000

    Update main.py
    removed infinite loop in main branch

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def main():
     controller = Controller()
 
     # Proof of concept connection to show the twin server can send data
-    while True:
-        controller.twin.twin_server.check_for_response()
+    #while True:
+    #    controller.twin.twin_server.check_for_response()
 
 
 if __name__ == "__main__":
