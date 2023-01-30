66666291b01c539e126c4f703307d5c73fe7fb4a
Framework-Pop_os <charlierstubbs@gmail.com>
2023-01-30 18:22:41 +0000

    Skeleton functions for csv read and conversion

--- controller/controller.py
+++ controller/controller.py
@@ -3,12 +3,15 @@
 from rover.rover import RoverControl
 from twin.twin import TwinSystem
 
+import pandas as pd
+
 
 class Controller:
     def __init__(self):
         print("created a new controller")
         self.rover = RoverControl()
         self.twin = TwinSystem()
+        self.current_data = None
 
     def send_to_rover(self):
         """
@@ -32,5 +35,23 @@ class Controller:
     def receive_from_rover(self):
         """
         Function to digest rover input
+        """
+        pass
+
+
+    def offline_load_file(self, location) :
+        """
+        Function for loading offline file
+        :param location: current location of offline file
+        :return status: Returns status code based on load
+        :return data: Returns CSV data for conversion
+        """
+        pass
+
+    def csv_to_dataframe(self, file) -> pd.DataFrame:
+        """
+        Converts CSV file to pandas dataframe
+        :param file:
+        :return:
         """
         pass
