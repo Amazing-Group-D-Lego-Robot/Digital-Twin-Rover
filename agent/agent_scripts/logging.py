23bd8a3e97f7affae7a446c6d871bed5617d7fa3
sjw624-uoy <sam@samwalker.dev>
2023-03-06 14:53:23 +0000

    Fixed formatting issue in logging Parser progress

--- agent/agent_scripts/logging.py
+++ agent/agent_scripts/logging.py
@@ -126,7 +126,7 @@ def log_instruction(data_file, instruction_string):
 def log_sensor_data(data_file):
         global time_last_instruction
         front_r, front_g, front_b, front_intensity = front_color.get_rgb_intensity()
-        rear_r, rear_g, rear_b, rear_intensity = rear_color.get_rgb_intensity()""
+        rear_r, rear_g, rear_b, rear_intensity = rear_color.get_rgb_intensity()
         accelerometer_x, accelerometer_y, accelerometer_z = hub.status()['accelerometer']
         yaw, pitch, roll = hub.status()['yaw_pitch_roll']
         gyro_x, gyro_y, gyro_z = hub.status()['gyroscope']
@@ -153,11 +153,10 @@ def log_sensor_data(data_file):
                 steering_motor.get_position(),
                 driving_motor.get_position(),
                 force_sensor.get_force_newton(),
-                "\n"
         ]
         data_strings = [str(x) for x in data]
 
-        data_bytes = bytes("\n".join(data_strings), "utf-8")
+        data_bytes = bytes("\n".join(data_strings)+"\n", "utf-8")
         data_file.write(data_bytes)
         data_file.flush()
 
