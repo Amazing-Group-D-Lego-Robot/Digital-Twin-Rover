d5ae7872104fe497dc9918555770e4c45c53bed9
charlie-framework <charlierstubbs@gmail.com>
2023-03-15 17:43:35 +0000

    Test file for the dumb twin predictor

---
+++ twin/twin_designs/test_dumb_twin.py
@@ -0,0 +1,74 @@
+from dumb_twin import DumbPredictor
+from controller.test_input_data_parser import parse_input_data
+
+input_data = input_data = """I:MOTOR C 25 720
+5
+2
+1
+2
+4
+0
+2
+0
+1
+None
+-27
+67
+-997
+34
+3
+178
+0
+0
+0
+85
+207
+0
+I:MOTOR A 25 -10
+6
+1
+1
+0
+3
+0
+0
+0
+1
+None
+-28
+70
+-997
+34
+3
+178
+0
+0
+0
+85
+220
+0
+140
+1
+1
+0
+4
+0
+2
+0
+1
+None
+-27
+69
+-996
+34
+3
+178
+0
+0
+0
+85
+220
+0
+I:WAIT 1"""
+
+data = parse_input_data(input_data)
