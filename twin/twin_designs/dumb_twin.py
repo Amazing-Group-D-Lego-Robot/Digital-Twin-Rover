907aa0a6a6fba70b4dc1abe2ff27da4c4c430902
charlie-framework <charlierstubbs@gmail.com>
2023-03-15 16:58:56 +0000

    Dumb twin model creation

--- twin/twin_designs/dumb_twin.py
+++ twin/twin_designs/dumb_twin.py
@@ -2,23 +2,20 @@ import numpy as np
 import pandas as pd
 
 from twin.predictors.predictor import Predictor
+from twin.twin_model import TwinModel
 
+class DumbTwinModel(TwinModel):
+    def __init__(self):
+        super().__init__()
+
+        # predictor
+        self.predictor = DumbPredictor()
+
+        #sensors
 
 class DumbPredictor(Predictor):
     def __init__(self):
         super().__init__()
-        # self.headers = ["time", "front_r", "front_g", "front_b", "front_intensity", "rear_r", "rear_g", "rear_b",
-        #                 "rear_intensity", "distance_sensor", "accelerometer_x", "accelerometer_y", "accelerometer_z",
-        #                 "yaw",
-        #                 "pitch", "roll", "gyro_x", "gyro_y", "gyro_z", "steering_motor_position",
-        #                 "driving_motor_position",
-        #                 "force_sensor_newton"]
-        #
-        # self.last_df = pd.DataFrame(columns=self.headers)
-        # self.last_df = pd.concat(pd.DataFrame(0, index=self.last_df.columns),self.last_df)
-        # df2 = pd.DataFrame([[0] * df.shape[1]], columns=df.columns)
-
-        # self.last_df = self.last_df.concat(, ignore_index=True)
 
 
 
