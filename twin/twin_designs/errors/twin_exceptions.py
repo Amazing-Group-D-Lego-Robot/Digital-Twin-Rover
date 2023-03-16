10846ff01c4b0283e9d9f6465cff3fbda9d0383e
charlie-framework <charlierstubbs@gmail.com>
2023-03-16 11:26:15 +0000

    removal of obsolete passed variable

--- twin/twin_designs/errors/twin_exceptions.py
+++ twin/twin_designs/errors/twin_exceptions.py
@@ -1,5 +1,5 @@
 class MotorPortError(Exception):
     """Raised when the port referenced for instruction MOTOR is invalid"""
-    def __init__(self, port, message="Motor Port is not port A or C"):
+    def __init__(self, port):
         self.message = f"Port {port} is not a valid motor port"
         super().__init__(self.message)
