5598f0c14d225c08d4ef59f01f791dde24ccdc47
charlie-framework <charlierstubbs@gmail.com>
2023-03-16 11:27:46 +0000

    addition of acceleration sensor

---
+++ twin/sensors/acceleration_sensor.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from twin.sensors.sensor import Sensor
+from twin.twin_environment import TwinEnvironment
+from twin.twin_model import TwinModel
+
+
+class AccelerationSensor(Sensor):
+    def __init__(self, name: str, direction: np.array, position: np.array):
+        super().__init__(name)
+
+        # normalised direction vector (0,0,1) is the same direction as the agent is facing)
+        self.direction = direction
+        self.position = position  # offset from agent position
+        self.value = np.array([0, 0, 0])  # XYZ Values for colour sensor
+
+    def update(self, twin: TwinModel, environment: TwinEnvironment):
+        # TODO: add update functionality when we have a format for environments
+        pass
