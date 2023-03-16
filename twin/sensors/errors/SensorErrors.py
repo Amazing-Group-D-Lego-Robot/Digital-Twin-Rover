cc64421bf20159d8c3dec132c27ad3a9243f8bfb
charlie-framework <charlierstubbs@gmail.com>
2023-03-16 12:03:19 +0000

    Fixed sensor errors

--- twin/sensors/errors/SensorErrors.py
+++ twin/sensors/errors/SensorErrors.py
@@ -1,5 +1,22 @@
-class RGBValueSizeError(Exception):
+class RGBValueError(Exception):
     """Raised when shape of RGB array is incorrect"""
 
-    def __init__(self, shape, typ):
-        self.message = f"Shape of input RGB + intensity array must be a 1 x 4 numpy array\n Current shape is {shape} with type {typ}"
+    def __init__(self, name, shape, typ):
+        self.message = f"{name}: RGB + intensity must be a list length 4\n Current length is {shape} with type {typ}"
+        super().__init__(self.message)
+
+
+class AccelerationValueError(Exception):
+    """If list is not correct length or type raise this error"""
+
+    def __init__(self, name, shape, typ):
+        self.message = f"{name}: XYZ value must be a list list of length 4\n Current length is {shape} with type {typ}"
+        super().__init__(self.message)
+
+
+class DistanceValueError(Exception):
+    """If it is not None or an integer this error is raised"""
+
+    def __init__(self, name, value):
+        self.message = f"{name}: value for sensor must be NoneType or int, {value} is neither"
+        super().__init__(self.message)
