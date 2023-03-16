f09d308f801ac0a863fdce4476e99a2161aa382c
charlie-framework <charlierstubbs@gmail.com>
2023-03-16 11:28:15 +0000

    Changes to color sensor to encapsulate rgb and intensity into one sensor

--- twin/sensors/color_sensor.py
+++ twin/sensors/color_sensor.py
@@ -3,6 +3,7 @@ import numpy as np
 from twin.sensors.sensor import Sensor
 from twin.twin_environment import TwinEnvironment
 from twin.twin_model import TwinModel
+from twin.sensors.errors.SensorErrors import RGBValueSizeError
 
 
 class ColorSensor(Sensor):
@@ -12,7 +13,18 @@ class ColorSensor(Sensor):
         # normalised direction vector (0,0,1) is the same direction as the agent is facing)
         self.direction = direction
         self.position = position  # offset from agent position
+        self.rgb_value = np.array([0, 0, 0, 0])  # RGB Values and intensity for colour sensor
 
     def update(self, twin: TwinModel, environment: TwinEnvironment):
         # TODO: add update functionality when we have a format for environments
         pass
+
+    def _update_value(self, rgb_vals: np.array):
+        """
+        Update rgb values
+        :param rgb_vals: 1 x 4 array contain [[R,G,B,intensity]] as np array
+        """
+        if np.shape(1,4):
+            raise RGBValueSizeError(np.shape, type(rgb_vals))
+
+        self.rgb_value = np.ndarray.copy(rgb_vals)
